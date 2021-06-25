64e2cfd5b0c9963fe6b82f243bf86dc81c9d99f6
frekike22 <evelinjurinovics22@gmail.com>
2021-06-25 20:49:50 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
 // Import stylesheets
 import './style.css';
 
+function factorial(n:number):number{
+let i:number
+let fact:number=1;
+for(i=1; i<=n; i++){
+  fact *=i;
+}
+return fact;
+}
+
+class ToDo {
+  constructor(public description: string){
+
+  }
+}
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>${factorial(6)}</h1>`;
